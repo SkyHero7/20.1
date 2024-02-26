c7c5aff67749a3060d48153fb9436feac2f72ad9
SkyHero7 <102468550+SkyHero7@users.noreply.github.com>
2024-02-26 18:57:11 +0400

    Update populate_data.py

--- myapp/management/commands/populate_data.py
+++ myapp/management/commands/populate_data.py
@@ -1,4 +1,6 @@
 from django.core.management.base import BaseCommand
+from django.db import transaction
+from django.db.transaction import TransactionManagementError
 from myapp.models import Category, Product
 
 
@@ -6,10 +8,24 @@ class Command(BaseCommand):
     help = 'Populate database with initial data'
 
     def handle(self, *args, **kwargs):
-        # Здесь вы можете добавить логику для создания и сохранения объектов Category и Product
-        Category.objects.create(name="Category 1", description="Description 1")
-        Category.objects.create(name="Category 2", description="Description 2")
-        Product.objects.create(name="Product 1", description="Description 1", category_id=1, price=10.99)
-        Product.objects.create(name="Product 2", description="Description 2", category_id=2, price=20.99)
+        try:
+            with transaction.atomic():
+                """
+                Удаление записи в таблицах
+                """
+                Category.objects.all().delete()
+                Product.objects.all().delete()
 
-        self.stdout.write(self.style.SUCCESS('Successfully populated data'))
+                """
+                Создание новых записей
+                """
+                Category.objects.create(name="Category 1", description="Description 1")
+                Category.objects.create(name="Category 2", description="Description 2")
+                Product.objects.create(name="Product 1", description="Description 1", category_id=1, price=10.99)
+                Product.objects.create(name="Product 2", description="Description 2", category_id=2, price=20.99)
+
+            self.stdout.write(self.style.SUCCESS('Successfully populated data'))
+
+        except TransactionManagementError:
+            self.stdout.write(self.style.ERROR('Failed to populate data. Transaction rolled back.'))
+
